0a596dca38f0903eb9361c1521ebc8db3060aad6
Phoebe Lau <phoebelau7@gmail.com>
2017-12-04 16:41:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|season,holidays|
     puts "#{season.capitalize}:"
       holidays.map {|holiday, item_list|
-        puts "  #{holiday.to_s.split('_').map{|day| day.capitalize!}.join(' ') }: #{item_list.join(", ")}"
+        puts "  #{holiday.to_s.split('_').map{|day| day.capitalize!}.join}: #{item_list.join(", ")}"
 
         #{hol.to_s.split('_').map {|name| name.capitalize}.join(' ') }: #{list.join(", ")}"
         #puts "  #{holiday.capitalize}: #{item_list.join(", ")}"
